358a049b0e84148c374bef5c6ce96a6369583ff9
Freidenker <138236775+johnson14988@users.noreply.github.com>
2023-10-07 19:54:57 +0800

    Update start.py

--- start.py
+++ start.py
@@ -3,14 +3,6 @@ from query_lemma1 import search_from_lemma_list
 from query_lemma2 import search_lemma
 from text_process import count_words, recognize_entities, split_sentences
 
-print("Please select a function:")
-print(">1.annotate all verbs' lemma in a text")
-print(">2.count word frequency and so on (text process)")
-print(">3.query single lemma (using spaCy model)")
-print(">4.query single lemma (using corpora data)")
-print("---------------------------------------------")
-order = input("Enter index number\n>>>")
-
 
 def text_lemmatizer():
     original_file = input("Input absolute path of your target file:\n>>>")
@@ -30,14 +22,24 @@ def process_text():
     recognize_entities(input_file, entities_file)
     split_sentences(input_file, sent_file)
 
+while True:
+    print("Please select a function:")
+    print(">1.annotate all verbs' lemma in a text")
+    print(">2.count word frequency and so on (text process)")
+    print(">3.query single lemma (using spaCy model)")
+    print(">4.query single lemma (using corpora data)")
+    print("---------------------------------------------")
+    order = input("Enter index number (type "q" to exit)\n>>>")
 
-if order == "1":
-    text_lemmatizer()
-if order == "2":
-    process_text()
-if order == "3":
-    search_lemma()
-if order == "4":
-    search_from_lemma_list()
-
-input("Press any key to exit...\n>>>")
+    if order == "1":
+        text_lemmatizer()
+    elif order == "2":
+        process_text()
+    elif order == "3":
+        search_lemma()
+    elif order == "4":
+        search_from_lemma_list()
+    elif order == "q"
+        exit()
+    else:
+        print("Invalid input")
